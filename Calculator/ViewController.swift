146459e7f0dea95139a16b696671a3696d4b58aa
AaronXue <her8153101@yahoo.com.tw>
2019-09-28 21:14:29 +0800

    Calculate model

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    let game = Calculation.self
+    
+    @IBOutlet weak var outlet: UILabel!
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+    
+    var numOfSum = 0 { didSet { outlet.text = "\(numOfSum)" } }
+    
+    
 
 }
 
