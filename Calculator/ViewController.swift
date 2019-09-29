89bbcb657a992de2714ff8b5f3403c8b2da53213
AaronXue <her8153101@yahoo.com.tw>
2019-09-29 23:19:08 +0800

    func of adding is added

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,28 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    lazy var controller = Calculator()
+    
     @IBOutlet weak var outlet: UILabel!
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
 //        // Do any additional setup after loading the view.
 //    }
 
-    var numOfSum = 0 { didSet { outlet.text = "\(numOfSum)" } }
+    var numOfLabel = "0" { didSet { outlet.text = "\(numOfLabel)" } }
     
     @IBOutlet var numButtons: [UIButton]!
 
     @IBAction func touchNum(_ sender: UIButton) {
         if let txtNumber = numButtons.firstIndex(of: sender){
-            numOfSum = txtNumber
+            if controller.operation > 0{
+                controller.recordNumber = Double(txtNumber)
+            }else{
+                changeDisplayOfLabel(number: txtNumber)
+            }
         }
+        
     }
     
-    //    if let cardNumber = cardButtons.firstIndex(of: sender) {
-//        game.chooseCard(at: cardNumber)
-//        updateViewFromModel()
-//    }
+    @IBOutlet var opeatorButton: [UIButton]!
     
-    
+    @IBAction func touchOperator(_ sender: UIButton) {
+        controller.reset = true
+        if let opCode = opeatorButton.firstIndex(of: sender){
+            controller.recordNumber = Double(Int(numOfLabel)!)
+            switch opCode{
+            case 0:controller.addition(
+                num1: Int(controller.recordNumber),
+                num2: Int(controller.numOfSum))
+            default:
+                print("error")
+            }
+            numOfLabel = String(Int(controller.numOfSum))
+            print(numOfLabel,controller.numOfSum)
+        }
+    }
 
+    func changeDisplayOfLabel(number: Int){
+        if(controller.reset == true){
+            controller.reset = false
+            numOfLabel = "0"
+        }
+        if(numOfLabel == "0"){
+            numOfLabel = String(number)
+        }else{
+            numOfLabel += String(number)
+        }
+    }
 }
 
